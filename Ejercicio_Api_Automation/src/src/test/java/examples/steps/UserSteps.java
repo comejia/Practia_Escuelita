6333ae6151709bda04d86f0333f1072fb6978323
cdellarosa <cdellarosa@practia.global>
2019-03-14 17:19:19 -0300

    User test with steps

--- Ejercicio_Api_Automation/src/src/test/java/examples/steps/UserSteps.java
+++ Ejercicio_Api_Automation/src/src/test/java/examples/steps/UserSteps.java
@@ -1,6 +1,7 @@
 package examples.steps;
 
 import io.qameta.allure.Step;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -20,6 +21,39 @@ public class UserSteps extends BaseStep {
         Assert.assertEquals("Leanne Graham", anObject.get("name"));
         Assert.assertEquals("Bret", anObject.get("username"));
         Assert.assertEquals("Email validation failed", "ale@hotmail.com", anObject.get("email"));
+
+    }
+
+    public void validateUserData() throws ParseException
+    {
+        Object arrayObject = new JSONParser().parse(response_body);
+        JSONArray jsonArray = (JSONArray) arrayObject;
+
+        JSONObject user = (JSONObject) jsonArray.get(0); /*Data user */
+
+       JSONObject address = (JSONObject) user.get("address");
+       JSONObject company = (JSONObject) user.get("company");
+
+                                    /*Asserts*/
+        /*Users size*/
+        Assert.assertEquals("Failed to get users size",10, jsonArray.size());
+
+        /*Validate User*/
+        Assert.assertEquals("1",user.get("id").toString());
+        Assert.assertEquals("Leanne Graham",user.get("name"));
+        Assert.assertEquals("Bret",user.get("username"));
+
+        Assert.assertEquals("Kulas Light", address.get("street"));
+        Assert.assertEquals("Apt. 556", address.get("suite"));
+        Assert.assertEquals("Gwenborough", address.get("city"));
+        Assert.assertEquals("92998-3874", address.get("zipcode"));
+
+        Assert.assertEquals("1-770-736-8031 x56442",user.get("phone"));
+        Assert.assertEquals("hildegard.org",user.get("website"));
+
+        Assert.assertEquals("Romaguera-Crona",company.get("name"));
+        Assert.assertEquals("Multi-layered client-server neural-net",company.get("catchPhrase"));
+        Assert.assertEquals("harness real-time e-markets",company.get("bs"));
 
     }
 
--- Ejercicio_Api_Automation/src/src/test/java/workspace/UsersTest.java
+++ Ejercicio_Api_Automation/src/src/test/java/workspace/UsersTest.java
@@ -2,7 +2,9 @@ package workspace;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import examples.steps.UserSteps;
 import helpers.ClientManager;
+import helpers.ConfigHelper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -17,30 +19,21 @@ import javax.ws.rs.core.Response;
 
 public class UsersTest
 {
+    String testMethodURL;
+
+    public UsersTest()
+    {
+        this.testMethodURL = ConfigHelper.getUserURL();
+    }
+
     @Test
-    public void testGetUserList() throws ParseException {
-        String endpointUrl = "https://jsonplaceholder.typicode.com/users";
+    public void testGetUserList() throws ParseException
+    {
+        UserSteps usteps = new UserSteps();
 
-        //SEND GET REQUEST
-        Client client = ClientManager.getClient();
-        Response response = client.target(endpointUrl)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .header("Content-type", "application/json; charset=UTF-8")
-                .get();
-
-
-        //GET RESPONSE BODY AS JSON OBJECT
-        String response_body = response.readEntity(String.class);
-
-
-        Object arrayObject = new JSONParser().parse(response_body);
-        JSONArray jsonArray = (JSONArray) arrayObject;
-
-
-        JSONObject User2 = (JSONObject) jsonArray.get(1); /*Data user 2*/
-
-        Assert.assertEquals("Failed to get users size",10, jsonArray.size());
-        Assert.assertEquals("Ervin Howell", User2.get("name").toString());
+        //usteps.getRequest(this.testMethodURL);
+        usteps.getRequest("https://jsonplaceholder.typicode.com/users/");
+        usteps.validateUserData();
     }
 
 
