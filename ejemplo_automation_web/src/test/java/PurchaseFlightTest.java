9ceb0a0897422f2f093c45f1ad415ef19d4e1fd1
Cesar Andres Castro Mujica <cecastro@practia.global>
2019-03-18 15:47:17 -0300

    TC2 POM v2

--- ejemplo_automation_web/src/test/java/PurchaseFlightTest.java
+++ ejemplo_automation_web/src/test/java/PurchaseFlightTest.java
@@ -32,19 +32,20 @@ public class PurchaseFlightTest extends BaseTest {
 
         //TODO: llegar al POM todos los findelement, pedirle al POM el string o el dato para el assert
         BlazeDemo_ChooseFlight chooseFlightPage = new BlazeDemo_ChooseFlight(webDriver);
-        Assert.assertNotNull("Could not found flights", findElement(By.xpath("//h3[contains(text(),'Flights')]"), 2, false));
+        Assert.assertEquals("Could not found flights","Flights from Paris to Cairo: ",chooseFlightPage.getTitleChooseFlightPage());
         chooseFlightPage.setChooseFlight(FlightOptions.ONE.getFlightOptions());
 
         BlazeDemo_Form purchasePage = new BlazeDemo_Form(webDriver);
-        Assert.assertNotNull("Could not reserve flight", findElement(By.xpath("//h2[contains(text(),'Your flight from Paris to Cairo has been reserved')]"), 5, false));
+        Assert.assertEquals("Could not reserve flight","Your flight from Paris to Cairo has been reserved.",purchasePage.getTitleForm());
 
         PurchaseFormData client = new PurchaseFormData("Cesar", "Balvanera", "Buenos Aires", "Buenos Aires", "1081", "1234567891234", "11", "2020", "Cesar Castro");
         purchasePage.completeForm(client);
         purchasePage.purchaseFlight();
 
         // Go the next page
-        BlazeDemo_ConfirmationPage confirmationPage = new BlazeDemo_ConfirmationPage();
-        Assert.assertNotNull("Could not reserve flight", findElement(By.xpath("//h1[contains(text(),'Thank you')]"), 5, false));
+        BlazeDemo_ConfirmationPage confirmationPage = new BlazeDemo_ConfirmationPage(webDriver);
+
+        Assert.assertEquals("Could not buy flight","Thank you for your purchase today!",confirmationPage.getMessage());
 
     }
 
